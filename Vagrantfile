50647cf487607f40016867e7411a81a1efd9b264
John Ryan <jnyryan@gmail.com>
2014-07-10 18:40:29 +0100

    changed to oracle java for logstash-forwarder PKI integration

--- Vagrantfile
+++ Vagrantfile
@@ -27,9 +27,10 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
   #config.vm.network "private_network", type: "dhcp"
-  config.vm.network :forwarded_port, host: 50080, guest: 80
-  config.vm.network :forwarded_port, host: 58080, guest: 8080
-  config.vm.network :forwarded_port, host: 59200, guest: 9200
+  config.vm.network :forwarded_port, host: 50080, guest: 80 # Kibana
+  config.vm.network :forwarded_port, host: 58080, guest: 8080 # test Site
+  config.vm.network :forwarded_port, host: 58888, guest: 8888 # test Site
+  config.vm.network :forwarded_port, host: 59200, guest: 9200 # Elastic Search
 
   # Create a public network, which generally matched to bridged network.
   # Bridged networks make the machine appear as another physical device on
@@ -46,7 +47,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # the path on the host to the actual folder. The second argument is
   # the path on the guest to mount the folder. And the optional third
   # argument is a set of non-required options.
-  # config.vm.synced_folder "../data", "/vagrant_data"
+  config.vm.synced_folder "etc", "/host_etc"
 
   # Provider-specific configuration so you can fine-tune various
   # backing providers for Vagrant. These expose provider-specific options.
--- install.sh
+++ install.sh
@@ -16,88 +16,64 @@ echo 'deb http://packages.elasticsearch.org/logstash/1.4/debian stable main' | s
 apt-get update
 apt-get install -y git curl make unzip
 
-
-####################
-# Install java
-apt-get -y install openjdk-7-jre-headless # for OPEN Java
-# apt-get install -y oracle-java7-installer # for ORACLE Java
-java -version
-
 #####################
-# nstall nginx
+# Install nginx
+echo Installing nginx
 apt-get install -y nginx
 cp /vagrant/etc/nginx/nginx.conf /etc/nginx/sites-available/default
 service nginx restart
 
 ####################
+# Install java
+echo Installing Java
+#apt-get -y install openjdk-7-jre-headless # for OPEN Java
+apt-get install -y oracle-java7-installer # for ORACLE Java
+java -version
+
+####################
+# Installing logstash
+echo Installing Logstash
+apt-get install -y logstash=1.4.1-1-bd507eb
+mkdir -p /etc/pki
+cp /vagrant/etc/pki/* /etc/pki 
+cp /vagrant/etc/logstash/logstash.conf /opt/logstash.conf
+
+####################
 # Installing Elastic Search
+echo Installing Elastic Search
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.2.1.deb
 dpkg -i elasticsearch-1.2.1.deb
 cd /usr/share/elasticsearch
 bin/plugin -install lmenezes/elasticsearch-kopf
 cd -
-
 echo Starting ES Service
 update-rc.d elasticsearch defaults 95 10
 /etc/init.d/elasticsearch start
 
 ####################
 # Install Kibana
-
+echo Installing Kibana
 cd ~
 wget http://download.elasticsearch.org/kibana/kibana/kibana-latest.zip
 unzip kibana-latest.zip
-	#Open the Kibana configuration file for editing:
-
-	#sudo vi ~/kibana-latest/config.js
-	#In the Kibana configuration file, find the line that specifies the elasticsearch, and replace the port number (9200 by default) with 80:
-
-   		#elasticsearch: "http://"+window.location.hostname+":80",
-	#This is necessary because we are planning on accessing Kibana on port 80 (i.e. http://logstashserverpublic_ip/).
-
-	#We will be using Nginx to serve our Kibana installation, so let's move the files into an appropriate location.
-
 mkdir -p /var/www/kibana
 cp -R ~/kibana-latest/* /var/www/kibana/
 cp /vagrant/etc/kibana/config.js /var/www/kibana/config.js
 
 ####################
-# Installing logstash
-apt-get install -y logstash=1.4.1-1-bd507eb
-#cp /vagrant/etc/logstash/logstash.conf /etc/logstash/conf.d/logstash.conf
-#cp /vagrant/etc/logstash/logstash.conf /etc/logstash.conf
-#cp /vagrant/etc/logstash/logstash.conf /etc/logstash/server.conf
-
-sudo cp /vagrant/etc/logstash/indexer.conf /etc/logstash.conf
-sudo cp /vagrant/etc/logstash/indexer.conf /etc/logstash/indexer.conf
-sudo cp /vagrant/etc/logstash/indexer.conf /etc/logstash/server.conf
-sudo cp /vagrant/etc/logstash/indexer.conf /etc/logstash/conf.d/indexer.conf
-sudo cp /vagrant/etc/logstash/indexer.conf /etc/logstash/conf.d/logstash.conf
-
-
-#service logstash restart
-
-#curl -O https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.tar.gz
-#tar zxvf logstash-1.4.2.tar.gz
-#cp logstash-1.4.2 /opt/
-#~/logstash-1.4.2/bin/logstash -e 'input { stdin { } } output { stdout {codec => rubydebug} }'
-#~/logstash-1.4.2/bin/logstash -e 'input { stdin { } } output { elasticsearch { host => localhost } }'
-#~/logstash-1.4.2/bin/logstash -e 'input { file { path => "/var/log/nginx/**" start_position => beginning } } output { elasticsearch { host => localhost } }'
-
-
-#ß~/logstash-1.4.2/bin/logstash -e 'input { file { path => "/var/log/nginx/**" start_position => beginning } file { path => [ "/var/log/*.log", "/var/log/messages", "/var/log/syslog" ] } } output { elasticsearch { host => localhost } }'
-
- #sudo ~/logstash-1.4.2/bin/logstash -e 'input { file { path => "/var/log/nginx/**" start_position => beginning } file { path => [ "/var/log/*.log", "/var/log/messages", "/var/log/syslog" ]  start_position => beginning } } output { elasticsearch { host => localhost } }'
-
-####################
 # Set up Test-Site 
+echo Installing Test Web Site
 mkdir -p /var/www/test-site
 cp -R /vagrant/web/* /var/www/test-site 
 
-
+####################
+# Run Logstash
+#sudo /opt/logstash/bin/logstash -f /opt/logstash.conf
 echo done
 
 ####################
 # https://www.digitalocean.com/community/tutorials/how-to-use-logstash-and-kibana-to-centralize-and-visualize-logs-on-ubuntu-14-04
 # https://gist.github.com/shadabahmed/5486949
+
+
 
